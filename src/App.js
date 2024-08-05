bb2e31457ba3c02f8d8589e5cf2940c28303a2d5
Jason <etho414.1@gmail.com>
2024-08-04 19:21:49 -0600

    testing commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  let [textvalue, setTextValue] = useState();
+  var a = 1;
+
+  function clicked() {
+    console.log("test");
+    a++;
+    console.log("Current value: " + a)
+  }
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       { a === 1  && ( 
+         <label>
+         testing!! <input name="testinput" />
+
+        </label>
+        )}
+        <button className="cheese" type="submit" onClick={clicked} > click me daddy </button>
       </header>
     </div>
   );
